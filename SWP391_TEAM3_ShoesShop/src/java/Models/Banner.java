70c06dc935ccea655996162751ae826b664e650c
minhkm <MinhTNHE180070@fpt.edu.vn>
2024-11-01 17:16:52 +0700

    fixed banner

---
+++ SWP391_TEAM3_ShoesShop/src/java/Models/Banner.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Admin
+ */
+public class Banner {
+    private int bannerId;
+    private String image;
+    private String link;
+    public Banner(){}
+    public Banner(int bannerId, String image, String link) {
+        this.bannerId = bannerId;
+        this.image = image;
+        this.link = link;
+    }
+
+    public int getBannerId() {
+        return bannerId;
+    }
+
+    public void setBannerId(int bannerId) {
+        this.bannerId = bannerId;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+    
+}
